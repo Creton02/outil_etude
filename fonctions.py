68f27a1bb5a14fe88b72d7e61b02e7ced71c1960
Creton02 <99781191+Creton02@users.noreply.github.com>
2025-03-12 05:17:26 +0000

    mécanisme de save et classe menu

--- fonctions.py
+++ fonctions.py
@@ -2,25 +2,45 @@
 Retrouvez tous les fonctions ici
 """
 import pandas as pd
+import os
 
 def load_excel(folder_name:str):
-    path = "/workspaces/outil_etude/excel/" + folder_name
+    path = "/workspaces/outil_etude/excel/" + folder_name + ".xlsx"
     matiere = pd.read_excel(path)
     return matiere
 
 class MatiereEtude():
-    def __init__(self, matiere):
+    def __init__(self, matiere:str):
         self.matiere = load_excel(matiere)
+        save_file = "/workspaces/outil_etude/saves/" + matiere + ".json"
+        if not os.path.exists(save_file) or os.stat(save_file).st_size == 0:
+            print(f"Aucun fichier de  sauvegarde trouvé, création d'une nouvelle sauvegarde.")
+            with open(save_file, "w") as file:
+                json.dump(self.matiere.cote, file, indent=4)
+            self.cote = self.matiere.cote
+        else:
+            with open(save_file, "r") as file:
+                self.cote = json.load(file)
+        print(f"Chargement de la sauvegarde.")
 
 
-    def afficher_terme(self, index:int):
+    def poser_question(self, index:int):
         print(f"{self.matiere.loc[index, "terme"]}")
-
-
-    def afficher_definition(self, index:int):
+        input(f"      ---------       ")
         print(f"{self.matiere.loc[index, "definition"]}")
+        input(f"      ---------       ")
+        difficulte = 0
+        while difficulte is not in range(1, 4):
+            difficulte = int(input(f"Facile (1), modéré (2), difficile (3)"))
+            match difficulte:
+                case 1:
+                    self.cote.loc[index] = 2
+                case 2:
+                    self.cote.loc[index] = 1
+                case 3:
+                    self.cote.loc[index] = 0
         if self.matiere.loc[index, "detail"] != '""':
-            choix = int(input(f"Voulez vous en savoir plus? (1)"))
+            choix = int(input(f"(1)"))
             if choix == 1:
                 print(f"{self.matiere.loc[index, "detail"]}")
         
@@ -31,31 +51,37 @@ class MatiereEtude():
             affichage += "\n"
         return(affichage)
     
-
-class Question():
-    def __init__(self):
-        pass
-
-
-    def poser_question(self):
-        pass
-
     
-    def actualiser_cote(self):
+class Menu():
+    def __init__(self, paths:list, matiereetude:class):
+        self.paths = paths
+        self.matiereetude = MatiereEtude("feuille_note_biologie")
+    
+
+    def menu_principal(self):
+        print(f"----------- Outil de révision -----------")
+        print(f" 1 - Consulter les différentes matières\n 2 - Afficher une matière\n 3 - Réviser une matière")
+
+
+    def affichage_matiere(self):
+        pass
+
+
+    def reviser_matiere(self):
         pass
 
 
     def __str__(self):
-        return("print de question")
+        return "ceci est l'affichage du menu"
+
     
-
-
 def main():
-    biologie = MatiereEtude("feuille_note_biologie.xlsx")
+    liste_paths = ["feuille_note_biologie"]
+    biologie = MatiereEtude("feuille_note_biologie")
     
     biologie.afficher_terme(2)
     biologie.afficher_definition(2)
-
+    
 
 if __name__ == "__main__":
     main()
