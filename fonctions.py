7a867f0ffd118c55188928f9e70db3b31a9983e3
Creton02 <99781191+Creton02@users.noreply.github.com>
2025-03-12 06:19:44 +0000

    Gros progrès

--- fonctions.py
+++ fonctions.py
@@ -3,25 +3,41 @@ Retrouvez tous les fonctions ici
 """
 import pandas as pd
 import os
+import json
+import atexit
+import random as rd
 
 def load_excel(folder_name:str):
     path = "/workspaces/outil_etude/excel/" + folder_name + ".xlsx"
     matiere = pd.read_excel(path)
     return matiere
 
+
 class MatiereEtude():
     def __init__(self, matiere:str):
         self.matiere = load_excel(matiere)
-        save_file = "/workspaces/outil_etude/saves/" + matiere + ".json"
-        if not os.path.exists(save_file) or os.stat(save_file).st_size == 0:
+        self.save_file = "/workspaces/outil_etude/saves/" + matiere + ".json"
+        if not os.path.exists(self.save_file) or os.stat(self.save_file).st_size == 0:
             print(f"Aucun fichier de  sauvegarde trouvé, création d'une nouvelle sauvegarde.")
-            with open(save_file, "w") as file:
-                json.dump(self.matiere.cote, file, indent=4)
-            self.cote = self.matiere.cote
+            with open(self.save_file, "w") as file:
+                json.dump(self.matiere.cote.tolist(), file, indent=4)
+            self.cote = self.matiere.cote.tolist()
         else:
-            with open(save_file, "r") as file:
+            with open(self.save_file, "r") as file:
                 self.cote = json.load(file)
         print(f"Chargement de la sauvegarde.")
+
+
+    def save_cote(self):
+        with open(self.save_file, "w") as file:
+            json.dump(self.cote, file, indent=4)
+            
+
+    def ecraser_cote(self):#à retravailler
+        with open(self.save_file, "w") as file:
+            file.write("{}")
+        print(f"La sauvegarde à été écrasé!")
+        self.cote = self.matiere.cote.tolist()
 
 
     def poser_question(self, index:int):
@@ -30,20 +46,32 @@ class MatiereEtude():
         print(f"{self.matiere.loc[index, "definition"]}")
         input(f"      ---------       ")
         difficulte = 0
-        while difficulte is not in range(1, 4):
-            difficulte = int(input(f"Facile (1), modéré (2), difficile (3)"))
+        while difficulte not in range(1, 4):
+            try:
+                difficulte = int(input(f"Facile (1), modéré (2), difficile (3) "))
+            except ValueError:
+                difficulte = 3
             match difficulte:
-                case 1:
-                    self.cote.loc[index] = 2
-                case 2:
-                    self.cote.loc[index] = 1
-                case 3:
-                    self.cote.loc[index] = 0
+                case "1":
+                    self.cote[index] = 2
+                case "2":
+                    self.cote[index] = 1
+                case "3":
+                    self.cote[index] = 0
         if self.matiere.loc[index, "detail"] != '""':
-            choix = int(input(f"(1)"))
+            try:
+                choix = int(input(f"Plus de détail? (1) "))
+            except ValueError:
+                choix = 0
             if choix == 1:
                 print(f"{self.matiere.loc[index, "detail"]}")
-        
+        if self.matiere.loc[index, "exemple"] != '"':
+            try:
+                choix = int(input(f"Un exemple? (1) "))
+            except ValueError:
+                choix = 2
+            if choix == 1:
+                print(f"{self.matiere.loc[index, "exemple"]}")
     def __str__(self):
         affichage = ""
         for e in range(len(self.matiere)):
@@ -53,22 +81,48 @@ class MatiereEtude():
     
     
 class Menu():
-    def __init__(self, paths:list, matiereetude:class):
+    def __init__(self, paths:list):
         self.paths = paths
         self.matiereetude = MatiereEtude("feuille_note_biologie")
-    
-
-    def menu_principal(self):
-        print(f"----------- Outil de révision -----------")
-        print(f" 1 - Consulter les différentes matières\n 2 - Afficher une matière\n 3 - Réviser une matière")
-
+        atexit.register(self.matiereetude.save_cote)
 
     def affichage_matiere(self):
         pass
 
 
     def reviser_matiere(self):
-        pass
+        numero_revise = []
+        for e in range(len(self.matiereetude.matiere.cote)):
+            if self.matiereetude.matiere.cote[e] == 0:
+                self.matiereetude.poser_question(e)
+            elif self.matiereetude.matiere.cote[e] == 1:
+                random = rd.randint(2)
+                if random == 1:
+                    self.matiereetude.poser_question(e)
+
+        self.matiereetude.poser_question(2)
+
+
+    def menu_principal(self):
+        condition_sortie = False
+        while condition_sortie == False:
+            print(f"----------- Outil de révision -----------")
+            print(f" 1 - Consulter les différentes matières\n 2 - Afficher une matière\n 3 - Réviser une matière")
+            print(f" 4 - Écraser une sauvegarde\n 5 - Sortir")
+            choix = int(input(f"Action: "))
+            
+            match choix:
+                case 1:
+                    pass
+                case 2:
+                    self.affichage_matiere
+                case 3:
+                    self.reviser_matiere()
+                case 4:
+                    self.matiereetude.ecraser_cote()
+                case 5:
+                    condition_sortie = True
+        
 
 
     def __str__(self):
@@ -76,12 +130,14 @@ class Menu():
 
     
 def main():
+    
     liste_paths = ["feuille_note_biologie"]
-    biologie = MatiereEtude("feuille_note_biologie")
+    menu = Menu(liste_paths)
+    menu.menu_principal()
+    """biologie = MatiereEtude("feuille_note_biologie")
+    atexit.register(biologie.save_cote)
+    biologie.poser_question(2)
+    biologie.ecraser_cote"""
     
-    biologie.afficher_terme(2)
-    biologie.afficher_definition(2)
-    
-
 if __name__ == "__main__":
     main()
