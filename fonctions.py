f41d9cf19847dfba2d097e07fca6272d4fbc9e45
Creton02 <99781191+Creton02@users.noreply.github.com>
2025-03-11 20:45:44 -0400

    Update fonctions.py

--- fonctions.py
+++ fonctions.py
@@ -1,7 +1,39 @@
 """
 Retrouvez tous les fonctions ici
 """
+import pandas as pd
 
-class QuestionEtude():
+def load_excel(folder_name:str):
+    path = "excel_files/" + folder_name
+    matiere = pd.read_excel(path)
+    return matiere
+
+class MatiereEtude():
+    def __init__(self, matiere):
+        self.chapitre = matiere[""]
+
+
+class Question():
     def __init__(self):
         pass
+
+
+    def poser_question(self):
+        pass
+
+    
+    def actualiser_cote(self):
+        pass
+
+
+    def __str__(self):
+        return("print de question")
+    
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
